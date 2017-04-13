f064fafc74ff393e418ce865296c59a770555162
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-12 19:08:59 -0700

    Added visualisation

--- data_analysis/Marker_Handler.py
+++ data_analysis/Marker_Handler.py
@@ -5,7 +5,8 @@ Created on Apr 11, 2017
 '''
 import sys
 import cv2
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 from Video_Marker import Video_Marker
 from Bagfile_Handler import Bagfile_Handler
@@ -21,18 +22,18 @@ class Marker_Handler:
         image_marker = Video_Marker() 
         area_visualizer = Area_Visualizer(bagfile_handler,image_marker)
 
-        # create persistent aruco marker dict       
-                
+        # create persistent aruco marker dict                       
         self.play_video(bagfile_handler,image_marker)
-
-          
 
     def play_video(self,bagfile_handler,image_marker):
           
         paused_video = False
+        
+        persistent_markers = {}
+        
         while True:
             if not paused_video:
-                gray,_ = image_marker.get_next_image(bagfile_handler.get_image()) 
+                gray,markers,fov = image_marker.get_next_image(bagfile_handler.get_image()) 
             
             cv2.imshow('frame',gray)
             key = cv2.waitKey(1000/30) & 0xFF
@@ -40,7 +41,55 @@ class Marker_Handler:
                 break
             if key == ord(' '):
                 paused_video = not paused_video
-
+                
+            
+            img2 = np.ones((600,600,3), np.uint8)
+            
+            # This factor is rather arbitrary. The x,y results of the following
+            # calculation are approximately in between 0 and 8 and accordingly this
+            # factor is chosen
+            scale_factor = 300.0 * (1.0/8.0)
+            shift_factor = 300.0
+            
+            persistent_markers.update(markers)
+            
+            for marker_id in persistent_markers:
+                #x = marker['distance']
+                #print(x)#xy_marker_positions.append(())
+                marker_data = persistent_markers[marker_id]
+                
+                x,y = cv2.polarToCart(marker_data['distance'],marker_data['angle']-np.pi)
+                x[0] = x[0] * scale_factor + shift_factor
+                y[0] = y[0] * scale_factor + shift_factor
+                cv2.circle(img2,(x[0],y[0]),5,(0,0,255),-1)
+                
+                # draw viewport lines
+                x_orig,y_orig = cv2.polarToCart(0.0,0.0-np.pi)
+                x_dest,y_dest = cv2.polarToCart(8.0,0.0-np.pi)
+                x_orig[0] = x_orig[0] * scale_factor + shift_factor
+                y_orig[0] = y_orig[0] * scale_factor + shift_factor
+                x_dest[0] = x_dest[0] * scale_factor + shift_factor
+                y_dest[0] = y_dest[0] * scale_factor + shift_factor
+                
+                cv2.line(img2,(x_orig[0],y_orig[0]),(x_dest[0],y_dest[0]),(255,255,255),1)
+                
+                if(fov != 0.0):
+                    cv2.line(img2,(x_orig[0],y_orig[0]),(x_dest[0],y_dest[0]),(255,255,255),1)
+                
+                    x_orig_max,y_orig_max = cv2.polarToCart(0.0,fov-np.pi)
+                    x_dest_max,y_dest_max = cv2.polarToCart(8.0,fov-np.pi)
+                    x_orig_max[0] = x_orig_max[0] * scale_factor + shift_factor
+                    y_orig_max[0] = y_orig_max[0] * scale_factor + shift_factor
+                    x_dest_max[0] = x_dest_max[0] * scale_factor + shift_factor
+                    y_dest_max[0] = y_dest_max[0] * scale_factor + shift_factor
+                    
+                    cv2.line(img2,(x_orig_max[0],y_orig_max[0]),(x_dest_max[0],y_dest_max[0]),(255,255,255),1)
+                
+                
+                
+                
+            cv2.imshow('topView',img2)
+            cv2.moveWindow('topView',800,0)
 
 if len(sys.argv) < 2:
     print("Please provide the path to the bagfile")
--- data_analysis/Video_Marker.py
+++ data_analysis/Video_Marker.py
@@ -10,24 +10,26 @@ import sys
 import zed_parameter
 import numpy as np
 from _socket import AF_X25
+from cv2 import polarToCart
 
 class Video_Marker(object):
     
     capture_device = None
     board = Board()
+    markers = {}
 
     def __init__(self, capture_device=None):
         
         if capture_device != None:
             self.capture_device = capture_device
             
-            capture_device.set(cv2.CAP_PROP_FRAME_WIDTH,2560)
-            capture_device.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+            capture_device.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+            capture_device.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
         else:
             self.capture_device = capture_device    
             
        
-    def get_next_image(self, cv_image = None):
+    def get_next_image(self, cv_image=None):
         if(self.capture_device == None):
             return self.mark_next_image(cv_image)
         else:
@@ -39,88 +41,119 @@ class Video_Marker(object):
 
     def mark_next_image(self, cv_image, crop=False):
                 
-        height, width, channel  = cv_image.shape
+        height, width, channel = cv_image.shape
 
-        yMin=0
-        yMax=height
-        xMin=0
-        xMax = width/2
+        yMin = 0
+        yMax = height
+        xMin = 0
+        xMax = width / 2
         # Capture frame-by-frame
-        #ret, frame = self.capture_device.read()
+        # ret, frame = self.capture_device.read()
         frame = cv_image
     
         if(crop):
-            frame = frame[yMin:yMax,xMin:xMax] #
+            frame = frame[yMin:yMax, xMin:xMax]  
         
         aruco_dict = self.board.get_dictionary()
-        parameters =  aruco.DetectorParameters_create()
+        parameters = aruco.DetectorParameters_create()
     
         res = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
         
         corners = res[0]
         ids = res[1]
         gray = frame
-        if len(corners)>0:
+        fov = 0
+        markers = {}
+        
+        if len(corners) > 0:
             gray = aruco.drawDetectedMarkers(frame, corners)
             
             try:
-                rvec, tvec = aruco.estimatePoseSingleMarkers(corners,0.20,zed_parameter.cameraMatrix,zed_parameter.distCoeffs)
+                rvec, tvec = aruco.estimatePoseSingleMarkers(corners, 0.20, zed_parameter.cameraMatrix, zed_parameter.distCoeffs)
             except:
                 pass
             
-        #    try:
-            self.drawPointAtSingleMarker(gray,rvec[0][0],tvec[0][0],zed_parameter.cameraMatrix,zed_parameter.distCoeffs)
-        #    except:
-        #        print(sys.exc_info()[1])
-        #        pass
+            for i in range(0,len(rvec)):
+                distance, angle, fov = self.drawPointAtSingleMarker(gray, rvec[i][0], tvec[i][0], zed_parameter.cameraMatrix, zed_parameter.distCoeffs, ids[i])
+                # It is a bit uninuitive that these information are calculated in a "draw" function. Needs redesign
+                markers[str(ids[i])]={'distance':distance,'angle':angle}
+            
+        
     
-        return gray, res
+        return gray, markers, fov
     
     
     
     
-    def drawPointAtSingleMarker(self,image,rvec,tvec,camMat,camDist):
+    def drawPointAtSingleMarker(self, image, rvec, tvec, camMat, camDist, id):
         '''
         This method draws single markers and their distances 
         '''
     
         length = 0.2
-            
-        axisPoints = np.array([[-length/2, -length/2, 0],[-length/2, length/2, 0],[length/2, -length/2,0],[length/2,length/2, 0]])
-    
-        imgpts, jac  = cv2.projectPoints(axisPoints, rvec, tvec, camMat, camDist);
         
-        xy1 = (int(imgpts[0][0][0]),int(imgpts[0][0][1]))
-        xy2 = (int(imgpts[1][0][0]),int(imgpts[1][0][1]))
-        xy3 = (int(imgpts[2][0][0]),int(imgpts[2][0][1]))
-        xy4 = (int(imgpts[3][0][0]),int(imgpts[3][0][1]))
+        axisPoints = np.array([[-length / 2, -length / 2, 0], [-length / 2, length / 2, 0], [length / 2, -length / 2, 0], [length / 2, length / 2, 0]])
+    
+        imgpts, jac = cv2.projectPoints(axisPoints, rvec, tvec, camMat, camDist);
+        
+        xy1 = (int(imgpts[0][0][0]), int(imgpts[0][0][1]))
+        xy2 = (int(imgpts[1][0][0]), int(imgpts[1][0][1]))
+        xy3 = (int(imgpts[2][0][0]), int(imgpts[2][0][1]))
+        xy4 = (int(imgpts[3][0][0]), int(imgpts[3][0][1]))
         
         # distance D from our camera.
         # object width in pixels P
         # Focal length F
         
-        W = 0.2 # meter size of marker
+        W = 0.2  # meter size of marker
       
         x = xy1[0]
         x_ = xy2[0]
         y = xy1[1]
         y_ = xy2[1]
         
-        distance= self.get_distance_of_line(W, (x, y), (x_, y_), camMat, camDist)
+        distance, angle = self.get_distance_and_angle_of_line(W, (x, y), (x_, y_), camMat, camDist)
+        distance = np.round(distance,3)
+        angle = np.round(angle,3)
+        
+        # 8 is arbitrary and here approximately the biggest measured distance
+        text_zoomfactor = 1 - (distance/(8-distance))
+        if text_zoomfactor < 0.5:
+            text_zoomfactor = 0.5
+        if text_zoomfactor > 1:
+            text_zoomfactor = 1
+        
+        cv2.putText(image, str(angle), xy1, cv2.FONT_HERSHEY_SIMPLEX, text_zoomfactor, (255, 255, 255), 2)
+        #cv2.putText(image,str(id),xy2,cv2.FONT_HERSHEY_SIMPLEX, text_zoomfactor, (0,255,0),2)
+        
+      
+        height, width, channel = image.shape
+        
+        fov = 2*np.arctan(width/(2*(camMat[0][0] + camMat[1][1]) / 2))
+        
+        return distance, angle, fov
 
-        cv2.putText(image,str(distance),xy1,cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,255,255),2)
-        #cv2.putText(image,str((F_y/P_y)),xy2,cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,255,0),2)
-
-    def get_distance_of_line(self, real_object_width_m, (px,py),(px_,py_),camMat,camDist):
+    def get_distance_and_angle_of_line(self, real_object_width_m, (px, py), (px_, py_), camMat, camDist):
+        '''
+        Returns distance to a line, given by px,py and px_,py_ and the angle in degree to 
+        the point px,xy
+        '''
                        
         # The focal length is averaged over fx and fy, which might decrease
         # the accuracy. This could be corrected in the future
-        F = (camMat[0][0]+camMat[1][1])/2   
-        P_x = (px_-px)
-        P_y = (py_-py)     
-        P = np.hypot(P_x,P_y)
+        F = (camMat[0][0] + camMat[1][1]) / 2   
+        P_x = (px_ - px)
+        P_y = (py_ - py)     
+        P = np.hypot(P_x, P_y)
         
         distance = (real_object_width_m * F) / P
         
-        return distance
-
+        x_mid = camMat[0][2]
+        y_mid = camMat[1][2]        
+        
+       
+        
+        #angle =  np.rad2deg(np.arctan2(y_mid - py, x_mid - px))
+        angle =  np.arctan2(y_mid - py, x_mid - px)
+        
+        return distance, angle
