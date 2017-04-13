860f357932d4d5f385d39b2dd4bebd1d7aab3c07
Karl Zipser <karlzipser@berkeley.edu>
2017-04-13 15:15:04 -0700

    gacp

--- teg2/car_run_params.py
+++ teg2/car_run_params.py
@@ -41,6 +41,7 @@ solver_file_path = opjh("kzpy3/caf5/z2_color/solver_live.prototxt")
 weights_file_path = opjh("kzpy3/caf5/z2_color/z2_color.caffemodel")
 verbose = False
 
+use_caffe = False
 steer_gain = 1.0
 
 gyro_freeze_threshold = 500
