1a9a3461250a0f4794c6d827e2459782c82f5345
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-12 19:57:58 -0700

    Tidied up the code

--- data_analysis/Marker_Handler.py
+++ data_analysis/Marker_Handler.py
@@ -15,12 +15,13 @@ from Area_Visualizer import Area_Visualizer
 class Marker_Handler:
     
     detected_markers = {}
+    area_visualizer = None
     
     def __init__(self, arguments):
         
         bagfile_handler = Bagfile_Handler(arguments)
         image_marker = Video_Marker() 
-        area_visualizer = Area_Visualizer(bagfile_handler,image_marker)
+        self.area_visualizer = Area_Visualizer()
 
         # create persistent aruco marker dict                       
         self.play_video(bagfile_handler,image_marker)
@@ -29,7 +30,6 @@ class Marker_Handler:
           
         paused_video = False
         
-        persistent_markers = {}
         
         while True:
             if not paused_video:
@@ -41,56 +41,10 @@ class Marker_Handler:
                 break
             if key == ord(' '):
                 paused_video = not paused_video
-                
-            
-            img2 = np.ones((600,600,3), np.uint8)
-            
-            # This factor is rather arbitrary. The x,y results of the following
-            # calculation are approximately in between 0 and 8 and accordingly this
-            # factor is chosen
-            scale_factor = 300.0 * (1.0/8.0)
-            shift_factor = 300.0
-            turn_factor = np.deg2rad(110.0/2.0)+np.pi/2
             
             if not paused_video:
-                for marker in persistent_markers:
-                    persistent_markers[marker]['confidence']=persistent_markers[marker]['confidence']/2.0
-                persistent_markers.update(markers)
+                self.area_visualizer.visualize_markers(markers)
             
-            for marker_id in persistent_markers:
-                #x = marker['distance']
-                #print(x)#xy_marker_positions.append(())
-                marker_data = persistent_markers[marker_id]
-                
-                x,y = cv2.polarToCart(marker_data['distance'],marker_data['angle']-turn_factor)
-                x[0] = x[0] * scale_factor + shift_factor
-                y[0] = y[0] * scale_factor + shift_factor
-                cv2.circle(img2,(x[0],y[0]),5,(0,0,255*marker_data['confidence']),-1)
-                
-                # draw viewport lines
-                x_orig,y_orig = cv2.polarToCart(0.0,0.0-turn_factor)
-                x_dest,y_dest = cv2.polarToCart(8.0,0.0-turn_factor)
-                x_orig[0] = x_orig[0] * scale_factor + shift_factor
-                y_orig[0] = y_orig[0] * scale_factor + shift_factor
-                x_dest[0] = x_dest[0] * scale_factor + shift_factor
-                y_dest[0] = y_dest[0] * scale_factor + shift_factor
-                
-                cv2.line(img2,(x_orig[0],y_orig[0]),(x_dest[0],y_dest[0]),(255,255,255),1)
-                
-                fov = np.deg2rad(110)
-                    
-                x_orig_max,y_orig_max = cv2.polarToCart(0.0,fov-turn_factor)
-                x_dest_max,y_dest_max = cv2.polarToCart(8.0,fov-turn_factor)
-                x_orig_max[0] = x_orig_max[0] * scale_factor + shift_factor
-                y_orig_max[0] = y_orig_max[0] * scale_factor + shift_factor
-                x_dest_max[0] = x_dest_max[0] * scale_factor + shift_factor
-                y_dest_max[0] = y_dest_max[0] * scale_factor + shift_factor
-                
-                cv2.line(img2,(x_orig_max[0],y_orig_max[0]),(x_dest_max[0],y_dest_max[0]),(255,255,255),1)
-                
-                
-            cv2.imshow('topView',img2)
-            cv2.moveWindow('topView',800,0)
 
 if len(sys.argv) < 2:
     print("Please provide the path to the bagfile")
