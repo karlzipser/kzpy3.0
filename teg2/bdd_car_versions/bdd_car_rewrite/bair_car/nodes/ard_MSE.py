426e9624a6355e7d5861025e31d68252ce63bc86
Karl Zipser <karlzipser@berkeley.edu>
2017-04-20 12:40:50 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_rewrite/bair_car/nodes/ard_MSE.py
+++ teg2/bdd_car_versions/bdd_car_rewrite/bair_car/nodes/ard_MSE.py
@@ -7,14 +7,14 @@ lock = threading.Lock()
 
 def apply_steer_pwm_gain(steer_pwm,M):
     if steer_pwm >= M['steer_null']:
-        pwm = (M['steer_pwm']-M['steer_null']) * M['steer_gain'] + M['steer_null']
+        pwm = (steer_pwm-M['steer_null']) * M['steer_gain'] + M['steer_null']
     else:
         pwm = (M['steer_null']-steer_pwm) * M['steer_gain'] + M['steer_null']
     return pwm
 
 def apply_motor_pwm_gain(motor_pwm,M):
     if motor_pwm >= M['motor_null']:
-        pwm = (M['motor_pwm']-M['motor_null']) * M['motor_gain'] + M['motor_null']
+        pwm = (motor_pwm-M['motor_null']) * M['motor_gain'] + M['motor_null']
     else:
         pwm = (M['motor_null']-motor_pwm) * M['motor_gain'] + M['motor_null']
     return pwm
