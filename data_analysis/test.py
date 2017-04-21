b92c50a77299948685289dd6c258634621c013c6
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-20 21:11:18 -0700

    Added short test method

---
+++ data_analysis/test.py
@@ -0,0 +1,41 @@
+'''
+Created on Apr 12, 2017
+
+@author: picard
+'''
+import pickle
+import sys
+import cv2
+import aruco_code
+"""
+for i in range(1,len(sys.argv)):
+    print("-----------------")
+    print("File: " + sys.argv[i])
+    file = open(sys.argv[i], "rb" )
+    
+    while True:
+        try:
+            mylist = pickle.load( file )
+            print(mylist)
+        except:
+            break
+
+
+    print("#####################")
+"""
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, image = cap.read()
+        
+    cv2.imshow('frame',image)
+    key = cv2.waitKey(1000/30) & 0xFF
+    
+    if key == ord('q'):
+        break
+    
+    aruco_steer,aruco_motor,aruco_only = aruco_code.do_aruco(image,55.0,60.0)
+    
+    
+    
