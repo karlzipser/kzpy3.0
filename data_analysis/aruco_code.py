ed3ea0f7433a8a4ec975be33ae54025e562cdc25
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-21 16:45:58 -0700

    ...

--- data_analysis/aruco_code.py
+++ data_analysis/aruco_code.py
@@ -9,7 +9,7 @@ def do_aruco(cv_image,steering_cmd,motor_cmd,ar_params):
     crop = False
     cv_image, markers, safe_motor, safe_steer, evasion_needed = marker.process_next_image(crop,ar_params,cv_image) 
     
-    #cv2.imshow('frame 2',cv_image)
+    cv2.imshow('frame 2',cv_image)
     #cv2.moveWindow('frame 2',700,600)
     return safe_steer,safe_motor,evasion_needed
 
