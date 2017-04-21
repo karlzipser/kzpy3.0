dc3fbec685fbc7c73f6871b99e7dbf2b8804c25e
Karl Zipser <karlzipser@berkeley.edu>
2017-04-20 20:35:11 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_rewrite/bair_car/nodes/arduino_node.py
+++ teg2/bdd_car_versions/bdd_car_rewrite/bair_car/nodes/arduino_node.py
@@ -70,7 +70,8 @@ def arduino_master_thread():
                 print("*** Data foldername = "+rp.foldername+ '***')
 
         if reload_timer.check():
-            reload(kzpy3.teg2.bdd_car_versions.bdd_car_rewrite.runtime_params)
+            reload(rp)
+            #reload(kzpy3.teg2.bdd_car_versions.bdd_car_rewrite.runtime_params)
             #from kzpy3.teg2.bdd_car_versions.bdd_car_rewrite.runtime_params import *
             #model_name_pub.publish(std_msgs.msg.String(weights_file_path))
             reload_timer.reset()
