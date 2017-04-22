6ee4dd438202736f6919bc5d8c697060aca3777c
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-21 17:09:20 -0700

    added aruco branch

--- data_analysis/aruco_code.py
+++ data_analysis/aruco_code.py
@@ -10,6 +10,10 @@ def do_aruco(cv_image,steering_cmd,motor_cmd,ar_params):
     cv_image, markers, safe_motor, safe_steer, evasion_needed = marker.process_next_image(crop,ar_params,cv_image) 
     #try: 
     cv2.imshow('frame 2',cv_image)
+    key = cv2.waitKey(1000/30) & 0xFF
+    if key == ord('q'):
+        break
+                
     #except:
     #cv2.moveWindow('frame 2',700,600)
     return safe_steer,safe_motor,evasion_needed
