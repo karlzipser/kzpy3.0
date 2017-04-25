84a0c422a9e44351c2394df61a82d7deed5f445f
Karl Zipser <karlzipser@berkeley.edu>
2017-04-25 15:00:28 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_rewrite/runtime_params.py
+++ teg2/bdd_car_versions/bdd_car_rewrite/runtime_params.py
@@ -53,6 +53,7 @@ if False:
 ####################### specific car settings ################
 #
 if computer_name == 'Mr_Orange':
+	PID_min_max = [1.,2.]
 	#motor_gain = 1.0
 	pass
 if computer_name == 'Mr_Silver':
@@ -66,7 +67,7 @@ if computer_name == 'Mr_Yellow':
 	#motor_gain = 0.9
 	pass
 if computer_name == 'Mr_Black':
-	PID_min_max = [0.8,1.5]
+	#PID_min_max = [0.8,1.5]
 	#motor_gain = 1.0
 	pass
 if computer_name == 'Mr_White':
