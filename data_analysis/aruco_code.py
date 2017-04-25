c1283d31976c1b2c78f0fe60d23c967ef806a9a0
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-24 20:16:27 -0700

    test

--- data_analysis/aruco_code.py
+++ data_analysis/aruco_code.py
@@ -17,7 +17,7 @@ def do_aruco(cv_image,steering_cmd,motor_cmd,ar_params, crop=False):
     #cv2.moveWindow('test',0,500)
 
     #print("Hey: " + str(safe_motor) + " , " + str(safe_steer))
-
+    evasion_needed = True
     return safe_steer,safe_motor,evasion_needed
 
 if __name__ == '__main__':
