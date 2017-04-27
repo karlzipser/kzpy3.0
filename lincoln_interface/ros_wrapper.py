c2e0a3daa3170f72deca417555a4973df35db152
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-04-27 00:38:34 -0700

    Added little tweak to limit the steering wheel velocity.

--- lincoln_interface/ros_wrapper.py
+++ lincoln_interface/ros_wrapper.py
@@ -10,22 +10,28 @@ roslib.load_manifest('dbw_mkz_msgs')
 import rospy
 import std_msgs.msg
 import numpy as np
-
+from collections import deque
 from dbw_mkz_msgs.msg import SteeringCmd
+from timeit import default_timer as timer
 
+
+debug = False
 
 class Lilliput_steering():
 
     steer_pub = rospy.Publisher('/vehicle/steering_cmd', SteeringCmd, queue_size=10)
 
-    previous_steering_command = None
+    # The five previous steering commands are used to smooth the response. They
+    # are initialised by 49, because that is by definition, straight.
+    # A deque ist used for its efficient list-shift
+    previous_steering_commands = deque([49,49,49,49,49,49])
+    smoothing_vector = np.array([0.1,0.1,0.15,0.2,0.2,0.25])
+    last_execution_time = timer()
 
     def __init__(self):
         self.lilliput_steering()
 
-    def steering_callback(self,data):
-        #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-        
+    def steering_callback(self,steering_model_data):        
         '''
         The dbw_mkz SteeringCmd message looks like this:
         # Steering Wheel
@@ -45,64 +51,65 @@ class Lilliput_steering():
         uint8 count
         '''
         
-        steeringCmd_msg = SteeringCmd()
+        # Sometimes the neural net is faster, sometimes slower. Every time it processes an input
+        # image it will publish a steering command and that would affect the smoothing of the
+        # values. To produce a more stable behavior, a timer is used. 
+        time_now = timer()
+    
+        max_update_frequency = 5 #hz
+        time_passed = (time_now - self.last_execution_time)*100.0
         
-        # The original steering values are in between 0 and 100 where 100 is max left and 0 is max right.
-        # This is converted to the range -8.2 to 8.2
+        if time_passed*max_update_frequency > 100.0:
+            self.last_execution_time = time_now
         
-        steering_lilliput = data.data
-        steering_norm = steering_lilliput / 100.0
-        
-        steer_max_left = -8.2 # rad
-        steer_max_right = 8.2 # rad
-        
-        steer_range = np.abs(steer_max_left-steer_max_right)
-        
-        steer_output_rad = (steering_norm * steer_range) - (steer_range / 2.0)
-        
-        # Smooth the steering commands heavily
-        if(self.previous_steering_command == None):
-            self.previous_steering_command = steer_output_rad
+            steeringCmd_msg = SteeringCmd()
             
-        # First we calculate the absolute difference of two consecutive steering commands and divide it by the possible
-        # range to reach normed values
-        diff_values_normed = np.abs(steer_output_rad-self.previous_steering_command)/steer_range
-        
-        # If the difference in between values is high ( sudden movement ), the new value factor goes towards 0
-        # and the old value factor, its opposite, goes towards 1
-        new_value_factor = 1-diff_values_normed
-        old_value_factor = diff_values_normed
-        
-        # To avoid having no movement at all in emergency situations the factors are limited
-        if new_value_factor < 0.1:
-            new_value_factor = 0.1
-            old_value_factor = 0.9
+            # The original steering values are in between 0 and 100 where 100 is max left and 0 is max right.
+            # This is first smoothed over 5 previous values and then converted to the range -8.2 to 8.2
             
+            # input from the callback
+            steering_lilliput = steering_model_data.data
             
-        # To limit the influence of new values, the opper limits are also set
-        if new_value_factor > 0.5:
-            new_value_factor = 0.5
-            old_value_factor = 0.5
+            # The queue is emptied on the left
+            self.previous_steering_commands.popleft()
             
-        steer_output_rad = (steer_output_rad*new_value_factor + self.previous_steering_command*old_value_factor) 
-        
-        
-        self.previous_steering_command = steer_output_rad
-        
-        
-        steeringCmd_msg.steering_wheel_angle_cmd = steer_output_rad
-        
-        self.steer_pub.publish(steeringCmd_msg)
+            # The newest value added
+            self.previous_steering_commands.append(steering_lilliput)
+            
+            # The newest steering command is the weighted sum of the previous 6 input commands
+            # with a weighting vector, designed to change the value first by a large, later by a minor margin
+            steering_lilliput = np.sum(self.previous_steering_commands*self.smoothing_vector)
+            
+            # Norm to [0,1]
+            steering_norm = steering_lilliput / 100.0
+                    
+            # Regard DMZ limits
+            steer_max_left = -8.2 # rad
+            steer_max_right = 8.2 # rad
+            
+            # Observe the range of the DMZ limits ( to make it possible to change those values later )
+            steer_range = np.abs(steer_max_left-steer_max_right)
+            
+            # Map the normed steering values from lilliput to the dmz
+            steer_output_rad = (steering_norm * steer_range) - (steer_range / 2.0)
+            
+            # Put it into a message
+            steeringCmd_msg.steering_wheel_angle_cmd = steer_output_rad
+            
+            # and publish it.
+            self.steer_pub.publish(steeringCmd_msg)
     
     def lilliput_steering(self):
-        #rospy.init_node('lilliput_steering', anonymous=True)
+        if debug:
+            rospy.init_node('lilliput_steering', anonymous=True)
+                
         rospy.Subscriber("/bair_car/cmd/steer", std_msgs.msg.Int32, self.steering_callback)
         
-        
-        #rospy.spin()
+        if debug:
+            rospy.spin()
         
         
 
 
-
-#test = Lilliput_steering()
+if debug:
+    test = Lilliput_steering()
