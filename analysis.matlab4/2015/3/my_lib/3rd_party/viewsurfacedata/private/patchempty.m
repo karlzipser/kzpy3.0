7f3333ea86be66ec57733f99c3de47445becd5ca
Karl Zipser <karlzipser@hiMac.local>
2017-04-26 22:31:05 -0700

    gacp

--- analysis.matlab4/2015/3/my_lib/3rd_party/viewsurfacedata/private/patchempty.m
+++
@@ -1,7 +0,0 @@
-function f = patchempty
-
-% function f = patchempty
-%
-% draw and return empty patch.
-
-f = patch('Vertices',[],'Faces',[]);
--- analysis.matlab4/2015/3/my_lib/3rd_party/viewsurfacedata/viewsurfacedata_closegui.m
+++
@@ -1,8 +0,0 @@
-function viewsurfacedata_closegui
-
-% function viewsurfacedata_closegui
-%
-% safely close gui window.
-
-global VS_GUI;
-closesafe(VS_GUI);
